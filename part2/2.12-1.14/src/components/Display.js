d45113a8157d97ffb094f693b3c2468ba1254a09
BlueOrange <this.is.blueorange@gmail.com>
2020-04-18 15:34:51 +0545

    weather data - 2.14

--- part2/2.12-1.14/src/components/Display.js
+++ part2/2.12-1.14/src/components/Display.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const DisplaySingle = ({ countryData, matched }) => {
   const data = countryData.find((country) => country.name === matched);
   return (
     <div>
       <h1>{data.name}</h1>
-      <p>{data.capital}</p>
-      <p>{data.population}</p>
+      <p>Capital: {data.capital}</p>
+      <p>Population: {data.population}</p>
       <h2>Languages</h2>
       <ul>
         {data.languages.map((lang) => (
@@ -18,8 +19,43 @@ const DisplaySingle = ({ countryData, matched }) => {
   );
 };
 
+const DisplayWeather = ({ countryData, matched }) => {
+  const [load, setLoad] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  const data = countryData.find((country) => country.name === matched);
+  const api = process.env.REACT_APP_API_KEY;
+  const url = `http://api.weatherstack.com/current?access_key=${api}&query=${data.capital}`;
+  const weatherHook = () => {
+    axios.get(url).then((response) => {
+      setWeather(response.data);
+      setLoad(true);
+    });
+  };
+  useEffect(weatherHook, []);
+
+  // weather data isnt yet fetched
+  if (!load) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>Weather in {data.capital}</h2>
+      <p>Pressure: {weather.current.pressure}</p>
+      <p>Temperature: {weather.current.temperature}</p>
+      <img
+        src={weather.current.weather_icons[0]}
+        alt={data.capital + "weather"}
+        height="100px"
+      ></img>
+    </div>
+  );
+};
+
 const Display = ({ userInp, countryData, setCountry }) => {
   // return nothing is there is no user input
+
   if (!userInp) {
     return null;
   }
@@ -33,7 +69,12 @@ const Display = ({ userInp, countryData, setCountry }) => {
   }
 
   if (matched.length === 1) {
-    return <DisplaySingle countryData={countryData} matched={matched[0]} />;
+    return (
+      <div>
+        <DisplaySingle countryData={countryData} matched={matched[0]} />
+        <DisplayWeather countryData={countryData} matched={matched[0]} />
+      </div>
+    );
   }
 
   return (
