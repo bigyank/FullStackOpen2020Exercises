17105b366aeae75f4b0745be961ccbb0690f0cd9
BlueOrange <this.is.blueorange@gmail.com>
2020-07-06 14:49:13 +0545

    8.19

--- part8/src/components/Books.js
+++ part8/src/components/Books.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const GenreBtn = ({ genre }) => {
-  return <button>{genre}</button>;
+const GenreBtn = ({ genres, setFilter }) => {
+  return (
+    <div>
+      {genres.map((genre) => {
+        return (
+          <button key={genre} onClick={() => setFilter(genre)}>
+            {genre}
+          </button>
+        );
+      })}
+      <button onClick={() => setFilter("all")}>all</button>
+    </div>
+  );
 };
 
-const Books = ({ show, allBooks }) => {
-  if (!show) {
-    return null;
-  }
-
-  if (allBooks.loading) {
-    return <p>Loading...</p>;
-  }
-  const books = allBooks.data.allBooks;
-  console.log(books);
-
+const BooksTable = ({ books }) => {
   return (
     <div>
       <h2>books</h2>
@@ -35,10 +36,47 @@ const Books = ({ show, allBooks }) => {
           ))}
         </tbody>
       </table>
+    </div>
+  );
+};
 
-      {books.map((book) => {
-        return book.genres.map((genre) => <GenreBtn genre={genre} />);
-      })}
+const Books = ({ show, allBooks }) => {
+  const [filter, setFilter] = useState("all");
+
+  const getGenres = () => {
+    let allGenre = [];
+
+    // extracting all genres
+    books.map((book) => {
+      const genre = book.genres.map((genre) => genre);
+      return (allGenre = [...allGenre, ...genre]);
+    });
+
+    // removing dublicate genre
+    // returning set as array to use higher order functions on it
+    return [...new Set(allGenre)];
+  };
+
+  if (!show) {
+    return null;
+  }
+
+  if (allBooks.loading) {
+    return <p>Loading...</p>;
+  }
+
+  let books = allBooks.data.allBooks;
+  const genreSet = getGenres(books);
+
+  // return books according to selected genre
+  if (filter !== "all") {
+    books = books.filter((book) => book.genres.includes(filter));
+  }
+
+  return (
+    <div>
+      <BooksTable books={books} />
+      <GenreBtn genres={genreSet} setFilter={setFilter} />
     </div>
   );
 };
